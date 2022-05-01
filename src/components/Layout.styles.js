ecb5d80a87ae5dd824195a5a9cbc3431edd41c54
Anatolii Markovskii <anatoliimarkovskii@gmail.com>
2022-05-01 22:17:20 +0300

    add fix

--- src/components/Layout.styles.js
+++ src/components/Layout.styles.js
@@ -3,7 +3,7 @@ import styled from "@emotion/styled";
 export const Layout = styled.div`
     border-radius: 4px;
     width: 400px;
-    background-color: #dddddd;
+    background-color: #409995;
     padding: 5px 40px;
     margin: 50px auto;
 `;
